5470579d785a9e16f8c571be0a66d688315d9e25
Emmett Cornies <339761330@GoTVDSB.ca>
2025-07-01 17:11:42 -0400

    Add project files.

---
+++ Part 4.5 - Methods Assignment/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Part_4._5___Methods_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+            bool done = false;
+            while (!done)
+            {
+                Console.SetCursorPosition(20, 2);
+                Console.WriteLine("Welcome to my extremely cool menu screen for my methods programming assignment!");
+                Console.SetCursorPosition(20, 0);
+                Console.WriteLine("-------------------------------------------------------------------------------");
+
+            }
+        }
+    }
+}
